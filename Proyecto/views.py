857dedefb5a60f9964f4fcad0c9e7d948cc37590
Jose Miguel Romero} <rjosemiguel787@gmail.com>
2023-09-17 20:04:39 -0500

    Stock

--- Proyecto/views.py
+++ Proyecto/views.py
@@ -15,23 +15,23 @@ def producto(request):
         return render(request,'Producto/insertar.html')
 def viewP(request):
     viewA=connection.cursor()
-    viewA.execute("select producto.*, vencimiento.fechaV from producto inner join vencimiento on vencimiento.id_producto=producto.id_producto where producto.Estado=0;")
+    viewA.execute("select producto.*, vencimiento.fechaV,vencimiento.LoteP,vencimiento.Cantidad from producto inner join vencimiento on vencimiento.id_producto=producto.id_producto where producto.Estado=0;")
     viewI=connection.cursor()
-    viewI.execute("select producto.*, vencimiento.fechaV from producto inner join vencimiento on vencimiento.id_producto=producto.id_producto where producto.Estado=1;")
+    viewI.execute("select producto.*, vencimiento.fechaV,vencimiento.LoteP,vencimiento.Cantidad from producto inner join vencimiento on vencimiento.id_producto=producto.id_producto where producto.Estado=1;")
     return render(request,'Producto/lista.html',{'productoA':viewA,'productoI':viewI})
 
-def update(request,id):
+def update(request,id,lote):
     if request.method=="POST":
         if request.POST.get('codigo') and request.POST.get('nombre') and  request.POST.get('precioC') and request.POST.get('precioT') and request.POST.get('gramo_litro') and request.POST.get('cantidad') and request.POST.get('vencimiento') and request.POST.get('lote') and request.POST.get('Max') and request.POST.get('Min'):
             ##Captura la infromacion, conecte a la base de datos y ejecute el insert
             insert=connection.cursor()
-            insert.execute("UPDATE producto SET id_producto="+request.POST.get('codigo')+",Nombre='"+request.POST.get('nombre')+"',PrecioC="+request.POST.get('precioC')+",PrecioT="+request.POST.get('precioT')+",GramoLitro='"+request.POST.get('gramo_litro')+"',Cantidad="+request.POST.get('cantidad')+",Fecha_Modificacion=now(),Lote='"+request.POST.get('lote')+"',Max="+request.POST.get('Max')+",Min="+request.POST.get('Min')+" where id_producto="+str(id)+";")
+            insert.execute("UPDATE producto SET id_producto="+request.POST.get('codigo')+",Nombre='"+request.POST.get('nombre')+"',PrecioC="+request.POST.get('precioC')+",PrecioT="+request.POST.get('precioT')+",GramoLitro='"+request.POST.get('gramo_litro')+"',Cantidad="+request.POST.get('cantidad')+",Fecha_Modificacion=now(),Lote='"+request.POST.get('lote')+"',Max="+request.POST.get('Max')+",Min="+request.POST.get('Min')+" where id_producto="+str(id)+" and Lote='"+str(lote)+"';")
             insert2=connection.cursor()
-            insert2.execute("UPDATE vencimiento SET  fechaV='"+request.POST.get('vencimiento')+"',id_producto="+request.POST.get('codigo')+",LoteP='"+request.POST.get('lote')+"' where id_producto="+str(id)+";")
+            insert2.execute("UPDATE vencimiento SET  fechaV='"+request.POST.get('vencimiento')+"',id_producto="+request.POST.get('codigo')+",LoteP='"+request.POST.get('lote')+"',Cantidad="+request.POST.get('cantidad')+" where id_producto="+str(id)+" and LoteP='"+str(lote)+"';")
             return redirect('/Producto/lista')
     else:
         consulta=connection.cursor()
-        consulta.execute("select producto.*, vencimiento.fechaV from producto inner join vencimiento on vencimiento.id_producto=producto.id_producto where producto.id_producto="+str(id)+";")
+        consulta.execute("select producto.*, vencimiento.fechaV,vencimiento.LoteP from producto inner join vencimiento on vencimiento.id_producto=producto.id_producto where producto.id_producto="+str(id)+" and vencimiento.LoteP='"+str(lote)+"';")
         return render(request,'Producto/actualizar.html',{'datos':consulta})
 def esatdoI(request,id):
     estadoI=connection.cursor()
@@ -41,3 +41,15 @@ def esatdoA(request,id):
     estadoA=connection.cursor()
     estadoA.execute("UPDATE producto SET Estado=0 where id_producto="+str(id)+"")
     return redirect('/Producto/lista')
+
+def Stock(request):
+    if request.method=="POST":
+        if request.POST.get('codigo') and  request.POST.get('precioC') and request.POST.get('precioT')  and request.POST.get('cantidad') and request.POST.get('vencimiento') and request.POST.get('lote'):
+            ##Captura la infromacion, conecte a la base de datos y ejecute el insert
+            insert=connection.cursor()
+            insert.execute("UPDATE producto SET PrecioC="+request.POST.get('precioC')+",PrecioT="+request.POST.get('precioT')+",Fecha_Modificacion=now() where id_producto="+request.POST.get('codigo')+";")
+            insert2=connection.cursor()
+            insert2.execute("Insert into vencimiento (fechaV,id_producto,LoteP,Cantidad) VALUES('"+request.POST.get('vencimiento')+"',"+request.POST.get('codigo')+",'"+request.POST.get('lote')+"',"+request.POST.get('cantidad')+");")
+            return redirect('/Producto/lista')
+    else:
+       return render(request,'Producto/Stock.html')
