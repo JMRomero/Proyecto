4cacfe4e55b32536b75d12ef747d0c414025f5cf
Sebastian Quintero <Sebastian Quintero@DESKTOP-H37M543>
2023-11-12 17:58:58 -0500

    diseño actual

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'public/'
 
 STATICFILES_DIRS=[
-    'Proyecto/public'
+    'Proyecto/static'
 ]
 
 # Default primary key field type
