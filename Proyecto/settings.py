df7d5eeb513786401ce49e674d3090ff2c29698e
Sebastian Quintero <Sebastian Quintero@DESKTOP-H37M543>
2023-11-14 16:52:33 -0500

    diseño 14/11/23

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'public/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[
     'Proyecto/static'
